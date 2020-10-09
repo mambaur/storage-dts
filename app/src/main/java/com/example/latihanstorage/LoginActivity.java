a06db1645bf82f4284e4f73bb73cfb706a09f301
Mambaur <alwiroziq1998@gmail.com>
2020-10-09 09:10:48 +0700

    merubah activity login dan menambah aktivity register

--- app/src/main/java/com/example/latihanstorage/LoginActivity.java
+++ app/src/main/java/com/example/latihanstorage/LoginActivity.java
@@ -2,13 +2,39 @@ package com.example.latihanstorage;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+    Button btnLogin, btnRegister;
+    EditText eUsername, ePassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        btnLogin = (Button) findViewById(R.id.btnLogin);
+        btnRegister = (Button) findViewById(R.id.btnRegister);
+        eUsername = (EditText) findViewById(R.id.eUsername);
+        ePassword = (EditText) findViewById(R.id.ePassword);
+
+        btnLogin.setOnClickListener(this);
+        btnRegister.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btnLogin:
+                break;
+            case R.id.btnRegister:
+                Intent intent = new Intent(this, RegisterActivity.class);
+                startActivity(intent);
+                break;
+        }
     }
 }
