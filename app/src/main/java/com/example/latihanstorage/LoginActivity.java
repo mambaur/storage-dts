778f7bea199be623facd9375d4aa9b1cb0de910f
Mambaur <alwiroziq1998@gmail.com>
2020-10-09 10:12:24 +0700

    Register done

--- app/src/main/java/com/example/latihanstorage/LoginActivity.java
+++ app/src/main/java/com/example/latihanstorage/LoginActivity.java
@@ -7,6 +7,14 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+
+import static android.provider.Telephony.Mms.Part.FILENAME;
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
     Button btnLogin, btnRegister;
@@ -17,6 +25,8 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        getSupportActionBar().setTitle("Login");
+
         btnLogin = (Button) findViewById(R.id.btnLogin);
         btnRegister = (Button) findViewById(R.id.btnRegister);
         eUsername = (EditText) findViewById(R.id.eUsername);
@@ -26,10 +36,59 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         btnRegister.setOnClickListener(this);
     }
 
+    void login(){
+        File file = new File(getFilesDir(), eUsername.getText().toString());
+        if(file.exists()){
+            StringBuilder stringBuilder = new StringBuilder();
+            try {
+                BufferedReader br = new BufferedReader(new FileReader(file));
+                String line = br.readLine();
+                while (line != null){
+                    stringBuilder.append(line);
+                    line = br.readLine();
+                }
+                br.close();
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+
+            String data = stringBuilder.toString();
+            String[] dataUser = data.split(";");
+            if (dataUser[1].equals(ePassword.getText().toString())){
+                simpanFileLogin();
+                Intent intent = new Intent(this, HomeActivity.class);
+                startActivity(intent);
+                finish();
+            }else{
+                Toast.makeText(this, "Password sesuai!", Toast.LENGTH_SHORT).show();
+            }
+        }else{
+            Toast.makeText(this, "User tidak ditemukan!", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    void simpanFileLogin(){
+        String isiFile = eUsername.getText().toString()+";"+ePassword.getText().toString();
+        File file = new File(getFilesDir(), FILENAME);
+        FileOutputStream fileOutputStream = null;
+        try {
+            file.createNewFile();
+            fileOutputStream = new FileOutputStream(file, false);
+            fileOutputStream.write(isiFile.getBytes());
+            fileOutputStream.flush();
+            fileOutputStream.close();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        Toast.makeText(this, "Login berhasil!", Toast.LENGTH_SHORT).show();
+        onBackPressed();
+    }
+
     @Override
     public void onClick(View v) {
         switch (v.getId()){
             case R.id.btnLogin:
+                login();
                 break;
             case R.id.btnRegister:
                 Intent intent = new Intent(this, RegisterActivity.class);
--- app/src/main/java/com/example/latihanstorage/RegisterActivity.java
+++ app/src/main/java/com/example/latihanstorage/RegisterActivity.java
@@ -3,12 +3,81 @@ package com.example.latihanstorage;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class RegisterActivity extends AppCompatActivity {
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+
+public class RegisterActivity extends AppCompatActivity implements View.OnClickListener {
+    Button btnRegister;
+    EditText eUsername, ePassword, eEmail, eName, eAsalSekolah, eAlamat;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        getSupportActionBar().setTitle("Register");
+
+        btnRegister = (Button) findViewById(R.id.btnRegister);
+        eUsername = (EditText) findViewById(R.id.eUsername);
+        ePassword = (EditText) findViewById(R.id.ePassword);
+        eEmail = (EditText) findViewById(R.id.eEmail);
+        eName = (EditText) findViewById(R.id.eAsalSekolah);
+        eAlamat = (EditText) findViewById(R.id.eAlamat);
+
+        btnRegister.setOnClickListener(this);
+    }
+
+    private boolean isValidation(){
+        if (eUsername.getText().toString().equals("") ||
+        ePassword.getText().toString().equals("") ||
+        eEmail.getText().toString().equals("") ||
+        eName.getText().toString().equals("") ||
+        eAsalSekolah.getText().toString().equals("") ||
+        eAlamat.getText().toString().equals("")){
+            return false;
+        }else{
+            return true;
+        }
+    }
+
+    void simpanFile(){
+        String isiFile = eUsername.getText().toString()+";"+
+                ePassword.getText().toString()+";"+
+                eEmail.getText().toString()+";"+
+                eName.getText().toString()+":"+
+                eAsalSekolah.getText().toString()+":"+
+                eAlamat.getText().toString();
+        File file = new File(getFilesDir(), eUsername.getText().toString());
+        FileOutputStream fileOutputStream = null;
+        try {
+            file.createNewFile();
+            fileOutputStream = new FileOutputStream(file, false);
+            fileOutputStream.write(isiFile.getBytes());
+            fileOutputStream.flush();
+            fileOutputStream.close();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        Toast.makeText(this, "Register berhasil!", Toast.LENGTH_SHORT).show();
+        onBackPressed();
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btnRegister:
+                if (isValidation()){
+                    simpanFile();
+                }else{
+                    Toast.makeText(this, "Mohon lengkapi seluruh data!", Toast.LENGTH_SHORT).show();
+                }
+                break;
+        }
     }
 }
